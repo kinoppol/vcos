0c01f7220e1a5f5420c31a850d3d9f04a00d867f
kinoppol <kinoppol@hotmail.com>
2025-07-20 20:16:34 +0700

    Update vcos.sql

--- install/vcos.sql
+++ install/vcos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 30, 2025 at 05:38 AM
+-- Generation Time: Jul 20, 2025 at 03:15 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
