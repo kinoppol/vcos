4b5578ef0258e43a30175a27f3fcce78740ed92b
kinoppol <kinoppol@hotmail.com>
2025-07-22 09:48:43 +0700

    Create time.php

---
+++ framework/helper/time.php
@@ -0,0 +1,11 @@
+<?php
+function xTimeAgo ($oldTime, $newTime) {
+$timeCalc = strtotime($newTime) – strtotime($oldTime);
+
+if ($timeCalc > (60*60*24)) {$timeCalc = round($timeCalc/60/60/24) . ” days ago”;}
+else if ($timeCalc > (60*60)) {$timeCalc = round($timeCalc/60/60) . ” hours ago”;}
+else if ($timeCalc > 60) {$timeCalc = round($timeCalc/60) . ” minutes ago”;}
+else if ($timeCalc > 0) {$timeCalc .= ” seconds ago”;}
+
+return $timeCalc;
+}
